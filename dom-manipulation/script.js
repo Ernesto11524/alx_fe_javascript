688a0c5914c45709721569f16825e4b11174a44c
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-08-30 17:40:37 +0000

    No description

---
+++ dom-manipulation/script.js
@@ -0,0 +1,61 @@
+const quoteDisplay = document.getElementById("quoteDisplay");
+const newQuote = document.getElementById("newQuote");
+
+let quotes = [
+  {
+    text: "Never give up",
+    category: "motivational"
+  },
+  {
+    text: "Always put yourself first",
+    category: "self-care"
+  },
+  {
+    text: "Focus on your goals",
+    category: "motivational"
+  },
+  {
+    text: "Everything will be fine",
+    category: "reassurance"
+  },
+  {
+    text: "God is in control",
+    category: "faith"
+  }
+];
+
+function addQuote(){
+    const newQuoteText = document.getElementById("newQuoteText").value;
+    const newQuoteCategory = document.getElementById("newQuoteCategory").value;
+    let addNewQuote = {
+        text: newQuoteText,
+        category: newQuoteCategory
+    }
+    quotes.push(addNewQuote)
+
+    for (let i = 0; i < quotes.length; i++){
+        console.log(quotes[i].text);
+        console.log(quotes[i].category);
+    }
+
+}
+
+function showRandomQuote(){
+    let num1 = Math.floor(Math.random() * quotes.length);
+    quoteDisplay.textContent = quotes[num1].text;
+    const addNewQuoteBtn = document.createElement("button");
+    addNewQuoteBtn.textContent = "Add New Quote";
+    addNewQuoteBtn.addEventListener("click", () => {
+    let showQuoteArea = document.createElement("div");
+    document.body.appendChild(showQuoteArea);
+    showQuoteArea.innerHTML = `
+        <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+        <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+        <button onclick="addQuote()">Add Quote</button>
+    `
+});
+    quoteDisplay.appendChild(addNewQuoteBtn);
+}
+
+newQuote.addEventListener("click", showRandomQuote);
+
